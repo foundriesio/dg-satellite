057b76cc6694581caa3ee27681ebeb48baf95589
Andy Doan <andy@foundries.io>
2025-06-18 14:32:58 -0500

    cmd: Add copyright marking
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// All rights reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
 package main
 
 import (

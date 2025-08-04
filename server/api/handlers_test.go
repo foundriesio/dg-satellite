8e8db496de35dd19c524fd936cb22cfa8c78a571
Andy Doan <andy@foundries.io>
2025-08-04 15:49:55 -0500

    server: Add skeleton for Rest API handlers
    This code introduces a way to define new REST API handlers and test
    them. Rather than spin up entire HTTP servers like we've done in the
    past, I'm leaning more on the httptest module to make this easier and
    faster.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ server/api/handlers_test.go
@@ -0,0 +1,70 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// All rights reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
+package api
+
+import (
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+
+	"github.com/foundriesio/dg-satellite/context"
+	"github.com/foundriesio/dg-satellite/server"
+	"github.com/foundriesio/dg-satellite/storage"
+	"github.com/foundriesio/dg-satellite/storage/api"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/require"
+)
+
+type testClient struct {
+	t   *testing.T
+	api *api.Storage
+	e   *echo.Echo
+	log *slog.Logger
+}
+
+func (c testClient) Do(req *http.Request) *httptest.ResponseRecorder {
+	req = req.WithContext(context.CtxWithLog(req.Context(), c.log))
+	rec := httptest.NewRecorder()
+	c.e.ServeHTTP(rec, req)
+	return rec
+}
+
+func (c testClient) GET(resource string, status int) []byte {
+	req := httptest.NewRequest(http.MethodGet, "/tmp", nil)
+	rec := c.Do(req)
+	require.Equal(c.t, http.StatusOK, rec.Code)
+	return rec.Body.Bytes()
+}
+
+func NewTestClient(t *testing.T) *testClient {
+	tmpDir := t.TempDir()
+	fsS, err := storage.NewFs(tmpDir)
+	require.Nil(t, err)
+	db, err := storage.NewDb(filepath.Join(tmpDir, "db.sqlite"))
+	require.Nil(t, err)
+	apiS, err := api.NewStorage(db, fsS)
+	require.Nil(t, err)
+
+	log, err := context.InitLogger("debug")
+	require.Nil(t, err)
+
+	e := server.NewEchoServer("api-test")
+	RegisterHandlers(e, apiS)
+
+	tc := testClient{
+		t:   t,
+		api: apiS,
+		e:   e,
+		log: log,
+	}
+	return &tc
+}
+
+func TestApi(t *testing.T) {
+	tc := NewTestClient(t)
+	data := tc.GET("/tmp", 200)
+	require.Equal(t, "OK", string(data))
+}

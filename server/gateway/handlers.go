390bf4e6c9541af1c01775143c1594d9666a88c0
Mike Sul <mike.sul@foundries.io>
2025-11-04 14:58:38 +0100

    gateway: Handle requests for app blobs

--- server/gateway/handlers.go
+++ server/gateway/handlers.go
@@ -4,11 +4,14 @@
 package gateway
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/foundriesio/dg-satellite/server"
 	storage "github.com/foundriesio/dg-satellite/storage/gateway"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type handlers struct {
@@ -39,4 +42,26 @@ func RegisterHandlers(e *echo.Echo, storage *storage.Storage) {
 	e.PUT("/system_info", h.hardwareInfo)
 	e.PUT("/system_info/config", h.akTomlInfo)
 	e.PUT("/system_info/network", h.networkInfo)
+
+	cr := e.Group("/v2")
+	// Handle requests like
+	// v2/msul-test/simple/manifests/sha256:d1e78dd150e58db366f850d93a5251e5074e72c5a5e54a0f1b5910f2fca8667b
+	// v2/msul-test/simple/blobs/sha256:<>
+	cr.HEAD("/:repo/:app/blobs/sha256\\::hash", h.blobHead)
+	cr.HEAD("/:repo/:app/manifests/sha256\\::hash", h.blobHead)
+
+	cr.GET("/:repo/:app/blobs/sha256\\::hash", h.blobGet)
+	cr.GET("/:repo/:app/manifests/sha256\\::hash", h.blobGet)
+}
+
+func (h *handlers) blobHead(c echo.Context) error {
+	fmt.Printf(">>> checkng if blob exists: %s\n", c.Param("hash"))
+	return c.NoContent(200)
+}
+
+func (h *handlers) blobGet(c echo.Context) error {
+	fmt.Printf(">>> getting blob: %s\n", c.Param("hash"))
+	// return blob content read from a local file storage
+	blobPath := filepath.Join(os.Getenv("APP_BLOBS_ROOT"), c.Param("hash"))
+	return c.File(blobPath)
 }

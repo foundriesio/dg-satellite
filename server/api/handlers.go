c07ed425f471e4582d424cbcbb01e2fc7cd7bcb3
Mike Sul <mike.sul@foundries.io>
2025-11-07 10:12:39 +0100

    handle app blob requests
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- server/api/handlers.go
+++ server/api/handlers.go
@@ -4,7 +4,10 @@
 package api
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"os"
+	"path/filepath"
 
 	"github.com/foundriesio/dg-satellite/auth"
 	"github.com/foundriesio/dg-satellite/server"
@@ -35,4 +38,26 @@ func RegisterHandlers(e *echo.Echo, storage *storage.Storage, authFunc auth.Auth
 	upd.GET("/:tag/:update/rollouts", h.rolloutList, requireScope(auth.ScopeDevicesR))
 	upd.GET("/:tag/:update/rollouts/:rollout", h.rolloutGet, requireScope(auth.ScopeDevicesR))
 	upd.PUT("/:tag/:update/rollouts/:rollout", h.rolloutPut, requireScope(auth.ScopeDevicesRU))
+
+	cr := e.Group("/v2")
+	// Handle requests like
+	// v2/msul-test/simple/manifests/sha256:d1e78dd150e58db366f850d93a5251e5074e72c5a5e54a0f1b5910f2fca8667b
+	// v2/msul-test/simple/blobs/sha256:<>
+	cr.HEAD("/:repo/:app/blobs/sha256\\::hash", h.blobHead, requireScope(auth.ScopeDevicesR))
+	cr.HEAD("/:repo/:app/manifests/sha256\\::hash", h.blobHead, requireScope(auth.ScopeDevicesR))
+
+	cr.GET("/:repo/:app/blobs/sha256\\::hash", h.blobGet, requireScope(auth.ScopeDevicesR))
+	cr.GET("/:repo/:app/manifests/sha256\\::hash", h.blobGet, requireScope(auth.ScopeDevicesR))
+}
+
+func (h *handlers) blobHead(c echo.Context) error {
+	fmt.Printf(">>> checkng if blob exists: %s\n", c.Param("hash"))
+	return c.NoContent(200)
+}
+
+func (h *handlers) blobGet(c echo.Context) error {
+	fmt.Printf(">>> getting blob: %s\n", c.Param("hash"))
+	// return blob content read from a local file storage
+	blobPath := filepath.Join(os.Getenv("APP_BLOBS_ROOT"), c.Param("hash"))
+	return c.File(blobPath)
 }

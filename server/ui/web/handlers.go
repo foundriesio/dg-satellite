ee28555386449922cfc594aef776e3c060185a60
Andy Doan <andy@foundries.io>
2025-12-05 21:08:05 -0600

    web: Add user listing page
    Signed-off-by: Andy Doan <andy@foundries.io>

--- server/ui/web/handlers.go
+++ server/ui/web/handlers.go
@@ -43,6 +43,7 @@ func RegisterHandlers(e *echo.Echo, storage *users.Storage, authProvider auth.Pr
 	e.GET("/auth/logout", h.authLogout, h.requireSession)
 	e.GET("/devices", h.devicesList, h.requireSession, h.requireScope(users.ScopeDevicesR))
 	e.GET("/settings", h.settings, h.requireSession)
+	e.GET("/users", h.usersList, h.requireSession, h.requireScope(users.ScopeUsersR))
 }
 
 type baseCtx struct {

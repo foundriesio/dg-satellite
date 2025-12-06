9bca28cd3d43f539c3c08ac3194356433ff605c5
Andy Doan <andy@foundries.io>
2025-12-05 21:08:05 -0600

    web: Add user listing page
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ server/ui/web/handlers_users.go
@@ -0,0 +1,24 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package web
+
+import (
+	"github.com/foundriesio/dg-satellite/storage/users"
+	"github.com/labstack/echo/v4"
+)
+
+func (h handlers) usersList(c echo.Context) error {
+	user, err := h.users.List()
+	if err != nil {
+		return h.handleUnexpected(c, err)
+	}
+	ctx := struct {
+		baseCtx
+		Users []users.User
+	}{
+		baseCtx: h.baseCtx(c, "Users", "users"),
+		Users:   user,
+	}
+	return h.templates.ExecuteTemplate(c.Response(), "users.html", ctx)
+}

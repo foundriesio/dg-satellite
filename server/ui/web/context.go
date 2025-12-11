8b63705d11884b6b2676a2f3c9b8f6f12f95dfe6
Andy Doan <andy@foundries.io>
2025-12-11 16:10:47 -0600

    web: Introduce helper to make REST API calls
    This uses the user session's HTTP client to make simple JSON GETs to the
    rest server.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- server/ui/web/context.go
+++ server/ui/web/context.go
@@ -4,6 +4,11 @@
 package web
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/foundriesio/dg-satellite/auth"
 	"github.com/foundriesio/dg-satellite/context"
 )
@@ -18,4 +23,30 @@ func CtxGetSession(ctx context.Context) *auth.Session {
 
 func CtxWithSession(ctx context.Context, session *auth.Session) context.Context {
 	return context.WithValue(ctx, ctxKeySession, session)
+}
+
+func CtxGetJson(ctx context.Context, resource string, result any) error {
+	s := CtxGetSession(ctx)
+
+	req, err := http.NewRequest("GET", s.BaseUrl+resource, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			context.CtxGetLog(ctx).Error("unable to close response body", "error", err)
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("unexpected status code: HTTP_%d: %s", resp.StatusCode, string(body))
+	}
+
+	return json.NewDecoder(resp.Body).Decode(result)
 }

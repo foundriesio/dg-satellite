f2c1bd37b3e7a5c9d5c2491d628bf52ae721694a
Andy Doan <andy@foundries.io>
2025-12-12 13:44:22 -0600

    web: Add ability to create API token
    Signed-off-by: Andy Doan <andy@foundries.io>

--- server/ui/web/handlers_users.go
+++ server/ui/web/handlers_users.go
@@ -4,7 +4,10 @@
 package web
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/foundriesio/dg-satellite/storage/users"
 	"github.com/labstack/echo/v4"
@@ -37,4 +40,49 @@ func (h handlers) usersAuditLog(c echo.Context) error {
 		return h.handleUnexpected(c, err)
 	}
 	return c.String(http.StatusOK, log)
+}
+
+func (h *handlers) userTokenCreate(c echo.Context) error {
+	session := CtxGetSession(c.Request().Context())
+
+	type TokenRequest struct {
+		Description string   `json:"description"`
+		Scopes      []string `json:"scopes"`
+		Expires     string   `json:"expires"`
+	}
+	var req TokenRequest
+	if err := c.Bind(&req); err != nil {
+		return EchoError(c, err, http.StatusBadRequest, "Could not parse request")
+	}
+
+	if len(req.Description) == 0 {
+		err := errors.New("token description is required")
+		return EchoError(c, err, http.StatusBadRequest, err.Error())
+	}
+	if len(req.Scopes) == 0 {
+		err := errors.New("at least one scope is required")
+		return EchoError(c, err, http.StatusBadRequest, err.Error())
+	}
+
+	scopes, err := users.ScopesFromSlice(req.Scopes)
+	if err != nil {
+		return EchoError(c, err, http.StatusBadRequest, fmt.Sprintf("Invalid scope: %s", err))
+	}
+
+	// Parse the ISO date string
+	expires, err := time.Parse(time.RFC3339, req.Expires)
+	if err != nil {
+		return EchoError(c, err, http.StatusBadRequest, "Invalid date format. Expected ISO date string. Got"+req.Expires)
+	}
+
+	// Validate that the expiration date is in the future
+	if expires.Before(time.Now()) {
+		err := fmt.Errorf("expiration date must be in the future. Got: %s", req.Expires)
+		return EchoError(c, err, http.StatusBadRequest, err.Error())
+	}
+	tok, err := session.User.GenerateToken(req.Description, expires.Unix(), scopes)
+	if err != nil {
+		return EchoError(c, err, http.StatusBadRequest, err.Error())
+	}
+	return c.String(http.StatusCreated, tok.Value)
 }

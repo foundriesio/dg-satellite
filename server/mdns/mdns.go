4adc78358ea887fe92c671e3173f24d326dbe60c
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2025-12-20 21:53:29 +0200

    mdns: register mdns services
    This does not work due to several bugs inside the hashicorp/mdns library:
    1. They don't respect the query ID; so DNS commands and higher level DNS servers reject its responses.
       I was able to patch the library to make it work with dig command at anycast addresses by fixing this.
    2. Their PTR record building does not suit our needs.
       No idea how this impacts the mdns, probably not a critical problem.
    3. When querying via the 224.0.0.251 multicast address, responses are lost into a black hole.
       I was not able to identify where things go awry.
       Probably need to rewrite the server part in a correct way; as it does not seem to be sending to a correct address.
       Expectedly, dig did not work with that address (it needs not work).
       But, neither getent, nor avahi-resolve were able to get the response, after they correctly queried my server.
       The nss switch was configured properly (otherwise, those tools would not even make the query).
       Apparently, any attempts to make requests to the services were unable to resolve locally.
       I was trying to configure smth like `test.local`, and it met the same problem.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- server/mdns/mdns.go
+++ server/mdns/mdns.go
@@ -9,20 +9,25 @@
 package mdns
 
 import (
+	"net"
 	"time"
 
 	"github.com/hashicorp/mdns"
+	dnsLib "github.com/miekg/dns"
 
 	"github.com/foundriesio/dg-satellite/context"
 )
 
 func newServer(ctx context.Context) dns {
 	log := context.CtxGetLog(ctx).With("server", "mdns")
+	mzone := &multiZone{}
 	return &mDNS{
 		context: ctx,
 		config: &mdns.Config{
 			Logger: context.StdLogAdapter(log, false),
+			Zone:   mzone, // An zone interface reference that mdns uses
 		},
+		mzone: mzone, // A zone struct that mDNS populates
 	}
 }
 
@@ -30,6 +35,7 @@ type mDNS struct {
 	context context.Context
 	config  *mdns.Config
 	server  *mdns.Server
+	mzone   *multiZone
 }
 
 func (s *mDNS) Start(quit chan error) {
@@ -71,6 +77,40 @@ func (s *mDNS) GetDnsName() string {
 }
 
 func (s *mDNS) register(service, domain, addr string, port uint16) error {
-	// TODO: This is where things get complex. Implemented in the next commit.
-	return nil
+	hostName := service + "." + domain
+	if srv, err := mdns.NewMDNSService(
+		// Instance and service names, used by the hashicorp/mdns to fill in the SRV record.
+		// We don't use them, as what they try to do is mimic a cloud.
+		// We use the host name to register A records instead.
+		// Maybe, we need to write our own zone logic to register a correct SRV record.
+		"satellite",
+		"satellite",
+		// Top-level domain name.
+		domain,
+		// Host name - this is how we want our service to actually resolve.
+		// HashiCorp assumes this is the local host name; that does not suit our use case.
+		hostName,
+		int(port),
+		[]net.IP{net.ParseIP(addr)},
+		nil, // optional text record, unused for us.
+	); err != nil {
+		return err
+	} else {
+		s.mzone.zones = append(s.mzone.zones, srv)
+		context.CtxGetLog(s.context).Info("Registered new mDNS service", "host", hostName, "addr", addr)
+		return nil
+	}
+}
+
+type multiZone struct {
+	zones []mdns.Zone
+}
+
+func (m *multiZone) Records(q dnsLib.Question) (res []dnsLib.RR) {
+	for _, z := range m.zones {
+		if rr := z.Records(q); len(rr) > 0 {
+			res = append(res, rr...)
+		}
+	}
+	return
 }

6492bd3679feb236deca881ce80f1cca52579f19
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2025-12-20 21:35:33 +0200

    mdns: auto-detect the server IP address from available interfaces
    A user can still provide the API address for the gateway manually.
    But, they can also provide the interface name, so that the IP address is auto-detected.
    If both IPv4 and IPv6 address are configured for the interface, the latter takes precedence.
    If a user specified neither address nor interface, auto-detect a non-loopback interface to use.
    If there is no non-loopback interfce or more than one non-loopback interface - require to provide either interface or address.
    Note that a user can still specify the loopback interface manually.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- server/mdns/intf.go
+++ server/mdns/intf.go
@@ -4,22 +4,49 @@
 package mdns
 
 import (
+	"errors"
 	"fmt"
+	"net"
 
 	"github.com/foundriesio/dg-satellite/context"
 	"github.com/foundriesio/dg-satellite/server"
 )
 
+var (
+	ErrNotFoundInterface   = errors.New("interface not found")
+	ErrZeroInterfaces      = errors.New("zero non-loopback interfaces found")
+	ErrAmbiguousInterfaces = errors.New("more than one non-loopback interfaces found")
+)
+
 type ServerParams struct {
+	// If Addr is not specified, use Intf to auto-detect the IP address.
+	// If Intf is not specified, auto-detect a non-loopback interface to use.
+	// If there is more than one non-loopback interface, return an error.
 	Addr string
+	Intf string
 	Port uint16
 	Tld  string
 }
 
 func NewServer(ctx context.Context, gatewayParams ServerParams) (server.Server, error) {
+	const errGateway string = "failed to configure gateway mDNS record: %w"
+	log := context.CtxGetLog(ctx)
 	srv := newServer(ctx)
+
+	gatewayAddr := gatewayParams.Addr
+	if len(gatewayAddr) != 0 {
+		log.Info("Using user provided IP address to set up mDNS gateway records", "addr", gatewayAddr)
+	} else {
+		var err error
+		if gatewayAddr, err = getLocalAddr(gatewayParams.Intf); err != nil {
+			return nil, fmt.Errorf(errGateway, err)
+		}
+		log.Info("Auto-detected IP address to set up mDNS gateway records",
+			"addr", gatewayAddr, "intf", gatewayParams.Intf)
+	}
+
 	for _, service := range []string{"api", "hub", "ostree"} {
-		if err := srv.register(service, gatewayParams.Tld, gatewayParams.Addr, gatewayParams.Port); err != nil {
+		if err := srv.register(service, gatewayParams.Tld, gatewayAddr, gatewayParams.Port); err != nil {
 			return nil, fmt.Errorf("failed to register mdns service %s: %w", service, err)
 		}
 	}
@@ -33,4 +60,60 @@ type configurator interface {
 type dns interface {
 	server.Server
 	configurator
+}
+
+func getLocalAddr(intfName string) (string, error) {
+	var (
+		interfaceList []net.Interface
+		allowLoopback bool
+	)
+	if len(intfName) > 0 {
+		allowLoopback = true
+		if intf, err := net.InterfaceByName(intfName); err != nil {
+			return "", ErrNotFoundInterface
+		} else {
+			interfaceList = append(interfaceList, *intf)
+		}
+	} else {
+		var err error
+		interfaceList, err = net.Interfaces()
+		if err != nil {
+			return "", err
+		}
+	}
+
+	var (
+		found bool
+		res   string
+	)
+	for _, intf := range interfaceList {
+		addrList, err := intf.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrList {
+			if ip, ok := addr.(*net.IPNet); ok {
+				if !allowLoopback && ip.IP.IsLoopback() {
+					continue
+				}
+				if found {
+					return "", ErrAmbiguousInterfaces
+				}
+				if res != "" {
+					if ip.IP.To4() == nil {
+						// If both IPv4 and IPv6 are set - prefer IPv4.
+						// This is what is usually needed in the link-local network.
+						// And it allows to make the mdns work faster.
+						continue
+					}
+				}
+				res = ip.IP.String()
+			}
+		}
+		found = res != ""
+	}
+	if !found {
+		return "", ErrZeroInterfaces
+	}
+	return res, nil
 }

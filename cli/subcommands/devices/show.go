307511ab8f07ee12ebcb42353cc503721cb2c140
Andy Doan <andy@foundries.io>
2026-01-24 10:51:24 -0600

    cli: Add devices-show command
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ cli/subcommands/devices/show.go
@@ -0,0 +1,98 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package devices
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/foundriesio/dg-satellite/cli/api"
+	rest "github.com/foundriesio/dg-satellite/storage/api"
+	"github.com/spf13/cobra"
+)
+
+var showCmd = &cobra.Command{
+	Use:   "show <uuid>",
+	Short: "Show details for a specific device",
+	Long:  `Display detailed information about a device`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		api := cmd.Context().Value(api.ContextKey).(*api.Api)
+		aktoml, _ := cmd.Flags().GetBool("aktoml")
+		hwinfo, _ := cmd.Flags().GetBool("hwinfo")
+		return showDevice(api, args[0], aktoml, hwinfo)
+	},
+}
+
+func init() {
+	DevicesCmd.AddCommand(showCmd)
+	showCmd.Flags().Bool("hwinfo", false, "Display hardware information")
+	showCmd.Flags().Bool("aktoml", false, "Display devices' reported configuration")
+}
+
+func showDevice(api *api.Api, uuid string, aktoml, hwinfo bool) error {
+	var device rest.Device
+	err := api.Get(fmt.Sprintf("/v1/devices/%s", uuid), &device)
+	cobra.CheckErr(err)
+
+	fmt.Printf("UUID:         %s\n", device.Uuid)
+	fmt.Printf("Target:       %s\n", device.Target)
+	fmt.Printf("Tag:          %s\n", device.Tag)
+	fmt.Printf("Is Prod:      %v\n", device.IsProd)
+
+	if device.CreatedAt > 0 {
+		fmt.Printf("Created At:   %s\n", time.Unix(device.CreatedAt, 0).Format("2006-01-02 15:04:05"))
+	}
+	if device.LastSeen > 0 {
+		fmt.Printf("Last Seen:    %s\n", time.Unix(device.LastSeen, 0).Format("2006-01-02 15:04:05"))
+	}
+
+	if device.UpdateName != "" {
+		fmt.Printf("Update Name:  %s\n", device.UpdateName)
+	}
+	if device.OstreeHash != "" {
+		fmt.Printf("OSTree Hash:  %s\n", device.OstreeHash)
+	}
+
+	if len(device.Apps) > 0 {
+		fmt.Printf("Apps:         %s\n", strings.Join(device.Apps, ", "))
+	}
+
+	if len(device.Labels) > 0 {
+		fmt.Println("\nLabels:")
+		for k, v := range device.Labels {
+			fmt.Printf("  %s: %s\n", k, v)
+		}
+	}
+
+	if device.NetInfo != "" {
+		fmt.Println("\nNetwork Info:")
+		fmt.Println(" ", device.NetInfo)
+	}
+
+	if aktoml && device.Aktoml != "" {
+		fmt.Println("\nAktoml:")
+		lines := strings.SplitSeq(device.Aktoml, "\n")
+		for line := range lines {
+			fmt.Printf(" | %s\n", line)
+		}
+	}
+
+	if hwinfo && device.HwInfo != "" {
+		fmt.Println("\nHardware Info:")
+		var hwinfo map[string]any
+		if err := json.Unmarshal([]byte(device.HwInfo), &hwinfo); err != nil {
+			fmt.Println("  (invalid hwinfo data):")
+			fmt.Println("  ", device.HwInfo)
+		} else {
+			hwinfoBytes, err := json.MarshalIndent(hwinfo, "  ", "  ")
+			cobra.CheckErr(err)
+			fmt.Println(" ", string(hwinfoBytes))
+		}
+	}
+
+	return nil
+}

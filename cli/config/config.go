efbe1db23916d1b55e5be7b8e5e40d5c4a1d616c
Andy Doan <andy@foundries.io>
2026-01-24 10:12:13 -0600

    cli: Add config logic
    A simple YAML config file where you can have multiple contexts so that
    multiple satellite servers can be used.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ cli/config/config.go
@@ -0,0 +1,78 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ActiveContext string `yaml:"active_context"`
+	Contexts      map[string]Context
+}
+
+type Context struct {
+	URL   string
+	Token string
+}
+
+// LoadConfig loads the CLI configuration from the default location
+func LoadConfig() (*Config, error) {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get config path: %w", err)
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("config file not found at %s", configPath)
+		}
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	return &config, nil
+}
+
+// GetContext retrieves the context by name. If name is empty, it returns the
+// configured active context.
+func (c *Config) GetContext(name string) (*Context, error) {
+	if c.ActiveContext == "" && name == "" {
+		return nil, fmt.Errorf("no default context set")
+	} else if name == "" {
+		name = c.ActiveContext
+	}
+
+	ctx, ok := c.Contexts[name]
+	if !ok {
+		return nil, fmt.Errorf("context '%s' not found", name)
+	}
+
+	if ctx.URL == "" {
+		return nil, fmt.Errorf("context '%s' has no URL configured", name)
+	}
+
+	if ctx.Token == "" {
+		return nil, fmt.Errorf("context '%s' has no token configured", name)
+	}
+
+	return &ctx, nil
+}
+
+func getConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".config", "satcli.yaml"), nil
+}

b20bf01f4c14b71d886743696ddf7e2c04492f03
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2026-02-16 19:07:39 +0000

    gateway: track config history timestamps and support if-modified-since on gateway
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- server/gateway/handlers_config.go
+++ server/gateway/handlers_config.go
@@ -6,6 +6,7 @@ package gateway
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,10 +24,23 @@ type ConfigFile struct {
 func (h handlers) configGet(c echo.Context) error {
 	req := c.Request()
 	ctx := req.Context()
+	log := CtxGetLog(ctx)
 	d := CtxGetDevice(ctx)
-	configs, err := d.GetConfigs()
+	configs, timestamp, err := d.GetConfigs()
 	if err != nil {
 		return EchoError(c, err, http.StatusInternalServerError, "failed to fetch config")
+	} else if timestamp == 0 {
+		return c.NoContent(http.StatusNoContent)
+	}
+
+	cts := time.Unix(timestamp, 0)
+	ifModifiedSince := req.Header.Get("If-Modified-Since")
+	if len(ifModifiedSince) > 0 {
+		if dts, err := time.Parse(time.RFC1123, ifModifiedSince); err != nil {
+			log.Warn("Unable to parse If-Modified-Since", "error", err, "if-modified-since", ifModifiedSince)
+		} else if dts.Before(cts) {
+			return c.String(http.StatusNotModified, "")
+		}
 	}
 
 	files := make(map[string]ConfigFile)
@@ -41,5 +55,6 @@ func (h handlers) configGet(c echo.Context) error {
 			files[k] = v
 		}
 	}
+	c.Response().Header().Set("Date", cts.Format(time.RFC1123))
 	return c.JSON(http.StatusOK, files)
 }

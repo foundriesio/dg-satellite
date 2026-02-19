5a81f54d507124609bf343acc6d616d7e07eedd5
Andy Doan <andy@foundries.io>
2026-02-19 14:43:11 -0600

    storage: Introduce fake db implementation
    Our CLI code does not use anything in the `storage` module. However, the
    golang compiler still sees the file and will try to build the sqllite
    depenendency. This fails when trying to cross-compile for other
    platforms as well as prevents us from making static binaries that are
    easy to distribute.
    
    This change fakes out the compiler to make it possible.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ storage/db_fake.go
@@ -0,0 +1,44 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+//go:build nodb
+
+package storage
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var ErrDbConstraintUnique = errors.New("sqllite.ErrConstraintUnique")
+
+func IsDbError(err error, code any) bool {
+	return false
+}
+
+type DbHandle struct {
+}
+
+func NewDb(dbfile string) (*DbHandle, error) {
+	return nil, nil
+}
+
+func (d DbHandle) Close() error {
+	return nil
+}
+
+func (d DbHandle) Prepare(name, query string) (stmt *sql.Stmt, err error) {
+	return nil, nil
+}
+
+func (d DbHandle) InitStmt(stmt ...DbStmtInit) error {
+	return nil
+}
+
+type DbStmt struct {
+	Stmt *sql.Stmt
+}
+
+type DbStmtInit interface {
+	Init(db DbHandle) error
+}

23eee8347cfcb332ce4c26694beb62bd01ea03e6
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2026-03-26 21:59:35 +0000

    cli: ability to upload configs tarball based on the configs directory
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- cli/subcommands/configs/upload.go
+++ cli/subcommands/configs/upload.go
@@ -16,7 +16,7 @@ import (
 )
 
 var uploadCmd = &cobra.Command{
-	Use:   "upload <configs.tgz>",
+	Use:   "upload <configs.tgz> | --dir <configs-dir>",
 	Short: "Upload configs",
 	Long: `Upload configs to the Satellite server.
 
@@ -24,46 +24,70 @@ var uploadCmd = &cobra.Command{
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		path := args[0]
+		isDir, _ := cmd.Flags().GetBool("dir")
 		api := api.CtxGetApi(cmd.Context())
-		cobra.CheckErr(uploadConfigs(api.Configs(), path))
+		cobra.CheckErr(uploadConfigs(api.Configs(), path, isDir))
 	},
 }
 
 func init() {
 	ConfigsCmd.AddCommand(uploadCmd)
+	uploadCmd.Flags().BoolP("dir", "d", false, "Archive a directory with configs to upload")
 }
 
-func uploadConfigs(capi api.ConfigsApi, path string) error {
-	var isGzip bool
-	switch ext := filepath.Ext(path); ext {
-	case ".tar":
-		break
-	case ".tar.gz", ".tgz":
-		isGzip = true
-	default:
-		return fmt.Errorf("supported file types are '.tar, .tar.gz, .tgz', but '%s' given", ext)
-	}
+func uploadConfigs(capi api.ConfigsApi, path string, isDir bool) error {
+	var reader io.ReadCloser
 
-	fd, err := os.OpenFile(path, os.O_RDONLY, 0)
-	if err != nil {
-		return fmt.Errorf("failed to read file '%s': %w", path, err)
-	}
-	defer fd.Close() //nolint:errcheck
+	if isDir {
+		if stat, err := os.Stat(path); err != nil {
+			return fmt.Errorf("failed to stat directory '%s': %w", path, err)
+		} else if !stat.Mode().IsDir() {
+			return fmt.Errorf("a '%s' is neither a directory nor a symlink to a directory", path)
+		}
 
-	if stat, err := fd.Stat(); err != nil {
-		return fmt.Errorf("failed to read file '%s': %w", path, err)
-	} else if !stat.Mode().IsRegular() {
-		return fmt.Errorf("a '%s' is neither a regular file nor a symlink to a regular file", path)
-	}
+		denyNonRegular := func(entry subcommands.ArchiveEntry) error {
+			mode := entry.Info.Mode()
+			if mode.IsDir() || mode.IsRegular() {
+				return nil
+			}
+			return fmt.Errorf("only regular files and directories are allowed for configs, but '%s' is neither", entry.Path)
+		}
 
-	var reader io.ReadCloser = fd
-	if !isGzip {
-		// Gzip raw tar files on-the-fly to save network traffic
-		reader = subcommands.GzipStream(func(gzipper io.Writer) error {
-			_, err = io.Copy(gzipper, fd)
-			return err
-		})
+		reader = subcommands.GzipStream(subcommands.TarStream(subcommands.ArchiveSourcer(path, denyNonRegular)))
 		defer reader.Close() //nolint:errcheck
+	} else {
+		var isGzip bool
+		switch ext := filepath.Ext(path); ext {
+		case ".tar":
+			break
+		case ".tar.gz", ".tgz":
+			isGzip = true
+		default:
+			return fmt.Errorf("supported file types are '.tar, .tar.gz, .tgz', but '%s' given", ext)
+		}
+
+		fd, err := os.OpenFile(path, os.O_RDONLY, 0)
+		if err != nil {
+			return fmt.Errorf("failed to read file '%s': %w", path, err)
+		}
+		defer fd.Close() //nolint:errcheck
+
+		if stat, err := fd.Stat(); err != nil {
+			return fmt.Errorf("failed to read file '%s': %w", path, err)
+		} else if !stat.Mode().IsRegular() {
+			return fmt.Errorf("a '%s' is neither a regular file nor a symlink to a regular file", path)
+		}
+
+		if !isGzip {
+			// Gzip raw tar files on-the-fly to save network traffic
+			reader = subcommands.GzipStream(func(gzipper io.Writer) error {
+				_, err = io.Copy(gzipper, fd)
+				return err
+			})
+			defer reader.Close() //nolint:errcheck
+		} else {
+			reader = fd
+		}
 	}
 
 	return capi.Upload(reader,
--- cli/subcommands/tgz.go
+++ cli/subcommands/tgz.go
@@ -4,11 +4,29 @@
 package subcommands
 
 import (
+	"archive/tar"
 	"compress/gzip"
+	"errors"
+	"fmt"
 	"io"
+	"iter"
+	"os"
+	"path/filepath"
 )
 
-type sourceWriter = func(io.Writer) error
+type (
+	ArchiveEntry struct {
+		RealPath string      // A real path the original file
+		Path     string      // A path to that file inside an archive
+		Info     os.FileInfo // A file info; if nil, will be taken from RealPath.
+	}
+
+	sourceWriter = func(io.Writer) error
+	fileSourcer  = iter.Seq2[*ArchiveEntry, error]
+	fileSkipper  = func(ArchiveEntry) error
+)
+
+var SkipEntry = errors.New("skip this entry") //nolint:staticcheck // Ignore ST1012 rule for error names, as we mimic filepath.SkipXXX.
 
 func GzipStream(source sourceWriter) io.ReadCloser {
 	// Allows to gzip any data provided by the source.
@@ -23,11 +41,111 @@ func GzipStream(source sourceWriter) io.ReadCloser {
 			_ = pw.CloseWithError(err)
 		}()
 		gw := gzip.NewWriter(pw)
-		if err = source(gw); err != nil {
-			_ = gw.Close()
-		} else {
-			err = gw.Close()
-		}
+		err = source(gw)
+		err = closeWithError(gw, err)
 	}()
 	return pr
+}
+
+func TarStream(source fileSourcer) sourceWriter {
+	// Allows to tar any sequence of files provided by the source into an output writer.
+	// A `source` is supposed to yield rairs of `[ArchiveEntry, nil]` to iterate over files, or `[nil, error]` to interrupt with error.
+	// Typical use is in conjunction with GzipStream and ArchiveSourcer as: `reader := GzipStream(TarStream(ArchiveSourcer(pathToDir)))`.
+	return func(writer io.Writer) (err error) {
+		tw := tar.NewWriter(writer)
+		defer func() {
+			err = closeWithError(tw, err)
+		}()
+
+		var entry *ArchiveEntry
+		for entry, err = range source {
+			if err != nil {
+				break
+			}
+			if err = entry.EnsureInfo(); err != nil {
+				break
+			}
+			var header *tar.Header
+			if header, err = tar.FileInfoHeader(entry.Info, ""); err != nil {
+				err = fmt.Errorf("failed to create tar header for '%s': %w", entry.Path, err)
+				break
+			}
+			header.Name = entry.Path
+			if err = tw.WriteHeader(header); err != nil {
+				err = fmt.Errorf("failed to write tar header for '%s': %w", entry.Path, err)
+				break
+			}
+
+			if !entry.Info.IsDir() {
+				var fd io.ReadCloser
+				if fd, err = os.Open(entry.RealPath); err != nil {
+					err = fmt.Errorf("failed to open '%s': %w", entry.Path, err)
+					break
+				}
+				defer fd.Close() //nolint:errcheck
+				if _, err = io.Copy(tw, fd); err != nil {
+					err = fmt.Errorf("failed to write '%s' to tar: %w", entry.Path, err)
+					break
+				}
+			}
+		}
+		return
+	}
+
+}
+
+func ArchiveSourcer(dir string, skipper ...fileSkipper) fileSourcer {
+	// Ranges through directory entries, yielding ArchiveEntry items.
+	// For each item, a Path is a RealPath relative to the `dir`.
+	// A caller can supply one or more `skipper` functions to filter out speficic entries.
+	return func(yield func(*ArchiveEntry, error) bool) {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			rel, err := filepath.Rel(dir, path)
+			if err != nil {
+				return err
+			}
+			if rel == "." {
+				// Skip root directory
+				return nil
+			}
+			entry := ArchiveEntry{RealPath: path, Path: rel, Info: info}
+			for _, skip := range skipper {
+				if err = skip(entry); err != nil {
+					if errors.Is(err, SkipEntry) {
+						err = nil
+					}
+					return err
+				}
+			}
+			if !yield(&entry, nil) {
+				return filepath.SkipAll
+			}
+			return nil
+		})
+		if err != nil {
+			_ = yield(nil, err)
+		}
+	}
+}
+
+func (entry *ArchiveEntry) EnsureInfo() (err error) {
+	if entry.Info == nil {
+		if entry.Info, err = os.Stat(entry.RealPath); err != nil {
+			err = fmt.Errorf("failed to stat '%s': %w", entry.Path, err)
+		}
+	}
+	return
+}
+
+func closeWithError(closer io.Closer, err error) error {
+	if err == nil {
+		err = closer.Close()
+	} else {
+		_ = closer.Close()
+	}
+	return err
 }

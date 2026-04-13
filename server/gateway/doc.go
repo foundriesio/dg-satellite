22a996f16dae0788a0216a43e7a8f45e94837588
Andy Doan <andy@foundries.io>
2026-04-13 15:39:02 -0500

    Enable building of swagger docs
    This bundles swagger documentation into our releases. I run this from my
    laptop with:
    
     PATH=${PATH}:${HOME}/go/bin make swagger
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ server/gateway/doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+// @title DG Satellite Gateway API
+// @description Device gateway API for mTLS-authenticated device communication.
+// @BasePath /
+
+package gateway
---
+++ server/ui/api/doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+// @title DG Satellite REST API
+// @description Management API for devices, updates, configs, and TUF metadata.
+// @BasePath /v1
+
+package api
